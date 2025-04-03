8ad3521d4b49f0a56af6a34f65a77f225bbb970b
rheaaa16 <rfabroborbe@gmail.com>
2025-04-03 21:34:20 +0800

    Create ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,16 @@
+PERF 2
+
+//If statement
+
+let age = prompt("Enter your Age:");
+
+
+if (age <= 13) {
+        console.log("You are a Child!"); 
+
+} else if (age >= 13 && age <= 19) {
+      console.log("You are a Teenager!");
+} else { 
+
+        console.log("You are an Adult!"); 
+}
